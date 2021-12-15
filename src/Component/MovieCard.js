c931f6d1a353ef1bc7129fbb368c1a7913ce612f
Saf25 <90209792+Saf25@users.noreply.github.com>
2021-12-15 12:46:28 +0100

    Update MovieCard.js

--- src/Component/MovieCard.js
+++ src/Component/MovieCard.js
@@ -4,19 +4,9 @@ import StarRatingComponent from "react-star-rating-component";
 import { Link } from "react-router-dom";
 export const MovieCard = ({ movie }) => {
   return (
-    <article className="MovieCard">
-      <Link to={`/movies/${movie.movieName}`}>
-        <img
-          style={{
-            borderRadius: "10px",
-            height: "400px",
-            maxHeight: "400px",
-            width: "302px",
-            maxWidth: "320px",
-          }}
-          src={movie.movieImg}
-          alt="none"
-        ></img>
+    <article>
+      <Link className="MovieCard" to={`/movies/${movie.movieName}`}>
+        <img className="style" src={movie.movieImg} alt="none"></img>
         <span>{movie.movieName}</span>
         <div className="starRating">
           <StarRatingComponent
